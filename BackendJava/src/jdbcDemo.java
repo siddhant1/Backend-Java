2ab995447fba48b705a3f9a3d86e0e345fc1be0b
siddhant1 <sidhant.manchanda@gmail.com>
2019-01-05 22:36:18 +0530

    CRUD Added:

--- BackendJava/src/jdbcDemo.java
+++ BackendJava/src/jdbcDemo.java
@@ -1,8 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ResourceBundle;
 
 public class jdbcDemo {
@@ -14,18 +10,36 @@ public class jdbcDemo {
 		Connection con = null;
 		con = DriverManager.getConnection(
 				   rb.getString("url"),rb.getString("userid"), rb.getString("password"));
+		con.setAutoCommit(false);
 		return con;
 	}
   
+	public static void write(int id, String name,int salary) throws ClassNotFoundException, SQLException {
+		Connection con = getConnection();
+		PreparedStatement pstmt = con.prepareStatement("insert into emp(id,name,salary) values (?,?,?)");
+		pstmt.setInt(1, id);
+		pstmt.setString(2,name);
+		pstmt.setInt(3, salary);
+		int result = pstmt.executeUpdate();
+		if(result>0) {
+			con.commit();
+		}else {
+			con.rollback();
+		}
+		
+		
+	}
+	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException  {
+//		write(10, "Ashubhai",4000);
 		boolean isFound = false;
 		Connection con = getConnection();
 		if(con != null) {
 			System.out.println("connected");
 		}
-		String query = "SELECT * from emp where name=?;";
+		String query = "SELECT * from emp";
 		PreparedStatement pstmt = con.prepareStatement(query);
-		pstmt.setString(1, "Siddhant");
+//		pstmt.setString(1, "Siddhant");
 		ResultSet rs = pstmt.executeQuery();
 		while(rs.next()) {
 			isFound = true;
